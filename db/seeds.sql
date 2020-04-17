bbcab7d0bc130a4ac6c13f0924146902b3f509fc
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-04-16 20:21:50 -0700

    Added doccuments needed for MVC and got it ready to deploy to heroku

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("Burger1", false);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("Burger2", false);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("Burger3", false);
